93162772977503a231c1e5ca6c77eca2cf2cd680
Applebarn <jperreaut35@gmail.com>
2015-02-28 23:56:51 +0100

    Added a sample new tweet type on Applebarn V3.0.0
    + added type_tweet == 2
    + added SpongeBob SquarePants
    + little stuff

--- applebarn_v3_0_0.py
+++ applebarn_v3_0_0.py
@@ -22,6 +22,12 @@ from twython import Twython
 # no
 
 #------------------------------------------------------------
+# Accès à l'API Twython
+#------------------------------------------------------------
+
+api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
+
+#------------------------------------------------------------
 # Processus de création du tweet
 #------------------------------------------------------------
 
@@ -35,11 +41,13 @@ if month == 8 and day == 28 and hour == 1: # Si nous sommes le 28 août à 1h
 	time.sleep(665) # Attend 11 minutes et 5 secondes
 	tweet = "@FriendlyPootis doit passer un super bon moment actuellement lel (me débranche pas stp)"
 	api.update_status(status=tweet)
+	print "Applebarn dit: " + tweet
 
 elif month == 7 and day == 28 and hour == 14: # Si nous sommes le 28 juillet à 14h
 	time.sleep(1805) # Attend 30 minutes et 5 secondes
 	tweet = "@FriendlyPootis doit passer un super bon moment actuellement lel (me débranche pas stp)"
 	api.update_status(status=tweet)
+	print "Applebarn dit: " + tweet
 
 elif month == 3 and day == 28 and hour == 10: # Si nous sommes le 28 mars à 10h
 	time.sleep(1505) # Attend 25 minutes et 5 secondes
@@ -47,6 +55,7 @@ elif month == 3 and day == 28 and hour == 10: # Si nous sommes le 28 mars à 10h
 	img = open('/home/pi/python/applebarn_assets/applebarn.png', 'rb')
 	tweet = "@FriendlyPootis Alors comme ça on a " + age + " ans ? Du coup... on baise ? ( ͡° ͜ʖ ͡°)"
 	api.update_status_with_media(status=tweet, media=img)
+	print "Applebarn dit: " + tweet
 
 elif month != 8 and month != 7 and month != 3 and day == 28 and hour == 1: # Si nous sommes le 28 autre que en mars, juillet, et août
 	time.sleep(665) # Attend 11 minutes et 5 secondes
@@ -54,26 +63,85 @@ elif month != 8 and month != 7 and month != 3 and day == 28 and hour == 1: # Si 
 	partieA = (random.choice(listeA))
 	tweet = "@FriendlyPootis" + espace + partieA
 	api.update_status(status=tweet)
+	print "Applebarn dit: " + tweet
 
 elif month == 2 and day == 14 and hour == 14: # Si nous sommes le 14 février à 14h
 	img = open('/home/pi/python/applebarn_assets/doigt.jpg', 'rb')
 	tweet = "Joyeuse Saint Valentin à tous !"
 	api.update_status_with_media(status=tweet, media=img)
+	print "Applebarn dit: " + tweet
 
 elif day == 5 and hour == 12: # Si nous sommes le 5 à 12h
 	tweet = "JE RAMÈNE UN PD, JE T'ATTACHE ET IL T'ENCULE !"
 	api.update_status(status=tweet)
+	print "Applebarn dit: " + tweet
 
 else: # Autrement
 	type_tweet = random.randint(1, 666) # Variable random servant à définir le type du tweet
 	if type_tweet == 1: # Tweet de présentation
 		listeA = ["Mon nom est Applebarn,","Je suis Applebarn,"] # Liste contenant 2 possibilités
 		partieA = (random.choice(listeA))
-		if hour == 0:
+		if hour == 0: # Si il est 0h
 			partieB = "et c'est l'heure du viol ( ͡° ͜ʖ ͡°)"
-		else:
-			listeB = ["et franchement je me fais chier là.","aimes-tu les films de gladiateurs ?","et j'adore le Lenny Face ( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)","on fait l'amour ? COMMENT ÇA NON ?","mais tu peux m'appeler Michel.","mon acuité visuelle est basée sur le mouvement.","et je tweet la vérité divine.","abonne toi, mets un j'aime, poste un commentaire gentil et fav la vidéo ! :D","on aime pas trop les gens comme vous par ici.","je ne vis que grâce à la R34.","c'est moi qui écrase les insectes.","ne t'inquiètes pas, je vais te montrer ma grange.","on va dans la grange ?"]
+		else: # Autrement
+			listeB = ["et franchement je me fais chier là.","aimes-tu les films de gladiateurs ?","et j'adore le Lenny Face ( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)","on fait l'amour ? COMMENT ÇA NON ?","mais tu peux m'appeler Michel.","mon acuité visuelle est basée sur le mouvement.","et je tweet la vérité divine.","abonne toi, mets un j'aime, poste un commentaire gentil et fav la vidéo ! :D","on aime pas trop les gens comme vous par ici.","je ne vis que grâce à la R34.","c'est moi qui écrase les insectes.","ne t'inquiètes pas, je vais te montrer ma grange.","on va dans la grange ?"] # Liste contenant 13 possibilités
 			partieB = (random.choice(listeB))
 		tweet = partieA + espace + partieB
 		api.update_status(status=tweet)
+		print "Applebarn dit: " + tweet
+	elif type_tweet == 2 # Tweet qui interpelle
+		opening = random.randint(1,4)
+		if opening == 4: # Si opening est égal à 4
+			# ASSISTEZ AU SPAM DU GÉNÉRIQUE DE BOB L'ÉPONGE !
+			bob42 = "Bob l'Éponge carrée !"
+			bob666 = "Bob l'Éponge caaaarrééééééeeeeeeeeee woh !"
+			bob1 = "Prêts les enfants ? @FriendlyPootis @LePlatos @Miyajyne @Poikopoik @EstrilChan @JosselinVanHaut @LeRainbaute @wolfermite @Syon35_Icarus"
+			bob2 = "Oui capitaine !"
+			bob3 = "J'ai pas entendu !"
+			bob4 = "Oui capitaaaaaiiine !"
+			bob5 = "Woooooooooo !"
+			bob6 = "Qui vit dans un ananas dans la mer ?"
+			bob7 = "Qui compte bien y faire carrière ?"
+			bob8 = "Si vous avez un souhait qui faut-il appeler ?"
+			bob9 = "Qui n'a pas peur des grands méchants poissons ?"
+			api.update_status(status=bob1)
+			time.sleep(5)
+			api.update_status(status=bob2)
+			time.sleep(5)
+			api.update_status(status=bob3)
+			time.sleep(5)
+			api.update_status(status=bob4)
+			time.sleep(5)
+			api.update_status(status=bob5)
+			time.sleep(5)
+			api.update_status(status=bob6)
+			time.sleep(5)
+			api.update_status(status=bob42)
+			time.sleep(5)
+			api.update_status(status=bob7)
+			time.sleep(5)	
+			api.update_status(status=bob42)
+			time.sleep(5)
+			api.update_status(status=bob8)
+			time.sleep(5)
+			api.update_status(status=bob42)
+			time.sleep(5)
+			api.update_status(status=bob9)
+			time.sleep(5)
+			api.update_status(status=bob42)
+			time.sleep(5)
+			api.update_status(status=bob42)
+			time.sleep(5)
+			api.update_status(status=bob42)
+			time.sleep(5)
+			api.update_status(status=bob42)
+			time.sleep(5)
+			api.update_status(status=bob666)
+		else: # Autrement
+			if hour == 0: # Si il est 0h
+				partieA = "C'est l'heure du viol ( ͡° ͜ʖ ͡°)"
+			elif hour == 12: # Si il est 12h
+				partieA = "Ah oui oui oui c'est la mi-teeeeeeeemps !"
+			else: # Autrement
+			# A venir lel
 		
